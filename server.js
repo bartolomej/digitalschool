4f57c68ad0842f147af18b968f4757c4e64cf7f0
bartolomej <bartolomej.kozorog@higroup.si>
2020-11-11 18:53:47 +0100

    fix

--- server.js
+++ server.js
@@ -3,36 +3,23 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT;
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// set form attribute enctype="application/json"
-// parse application/json
 app.use(bodyParser.json())
 
 app.post("/raw", function (req, res) {
-  if (req.is('application/json')) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.end(JSON.stringify(req.body, null, 2))
-  } else {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Ne podpiram tega tipa vsebine!');
-  }
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(JSON.stringify(req.body, null, 2));
 });
 
 app.post("/pretty", function (req, res) {
-  if (req.is('application/json')) {
-    res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/html');
+  if (typeof req.body === 'object') {
+    console.log(req.body)
     res.end(`
-  	<ul>
-      ${Object.keys(req.body).map(k => (`
-        <li>${k}: ${req.body[k]}</li>
-      `))}
-    </ul>
-  `);
+	  	<ul>${Object.keys(req.body).map(k => (`<li>${k}: ${req.body[k]}</li>`))}</ul>
+  	`);
   } else {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Ne podpiram tega tipa vsebine!');
+    res.end('Ne podpiram tega tipa!')
   }
 });
 
